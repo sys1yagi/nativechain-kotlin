eeb4baf08875ac07f229988dbf539efe6fe0a477
Toshihiro Yagi <sylc.yagi@gmail.com>
2018-01-06 00:09:52 +0900

    Separate GenesisBlock from NativeChain.

--- src/main/kotlin/com/sys1yagi/NativeChain.kt
+++ src/main/kotlin/com/sys1yagi/NativeChain.kt
@@ -1,5 +1,6 @@
 package com.sys1yagi
 
+import com.sys1yagi.util.GenesisBlock
 import com.sys1yagi.util.TimeProvider
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
@@ -9,36 +10,7 @@ import java.security.MessageDigest
 class NativeChain(val timeProvider: TimeProvider) {
     val logger: Logger = LoggerFactory.getLogger("NativeChain");
 
-    companion object {
-        val GENESIS_BLOCK = Block(
-            0,
-            "0",
-            1465154705,
-            "my genesis block!!",
-            "816534932c2b7154836da6afc367695e6337db8a921823784c14378abed4f7d7"
-        )
-    }
-
-    val blockchain = arrayListOf(GENESIS_BLOCK)
-
-    private fun calculateHash(
-        index: Long,
-        previousHash: String,
-        timestamp: Long,
-        data: String): String {
-        return MessageDigest.getInstance("SHA-256")
-            .digest("$index$previousHash$timestamp$data".toByteArray(StandardCharsets.UTF_8))
-            .joinToString(separator = "") { "%02X".format(it) }
-    }
-
-    private fun calculateHashForBlock(block: Block) = calculateHash(
-        block.index,
-        block.previousHash,
-        block.timestamp,
-        block.data
-    )
-
-    private fun getLatestBlock() = blockchain.last()
+    val blockchain = arrayListOf(GenesisBlock)
 
     fun generateNextBlock(blockData: String): Block {
         val previousBlock = getLatestBlock()
@@ -74,4 +46,23 @@ class NativeChain(val timeProvider: TimeProvider) {
             logger.debug("add failed")
         }
     }
+
+    private fun calculateHash(
+        index: Long,
+        previousHash: String,
+        timestamp: Long,
+        data: String): String {
+        return MessageDigest.getInstance("SHA-256")
+            .digest("$index$previousHash$timestamp$data".toByteArray(StandardCharsets.UTF_8))
+            .joinToString(separator = "") { "%02X".format(it) }
+    }
+
+    private fun calculateHashForBlock(block: Block) = calculateHash(
+        block.index,
+        block.previousHash,
+        block.timestamp,
+        block.data
+    )
+
+    private fun getLatestBlock() = blockchain.last()
 }
---
+++ src/main/kotlin/com/sys1yagi/util/GenesisBlock.kt
@@ -0,0 +1,11 @@
+package com.sys1yagi.util
+
+import com.sys1yagi.Block
+
+val GenesisBlock = Block(
+    0,
+    "0",
+    1465154705,
+    "my genesis block!!",
+    "816534932c2b7154836da6afc367695e6337db8a921823784c14378abed4f7d7"
+)
